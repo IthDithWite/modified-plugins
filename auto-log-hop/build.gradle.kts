4c9df7c3d9017c3a44aa4e18d7041dea795eee23
Davit Shalikiani <115648955+IthDithWite@users.noreply.github.com>
2024-02-03 22:03:06 +0400

    Fixed AutoLogHop

---
+++ auto-log-hop/build.gradle.kts
@@ -0,0 +1,18 @@
+version = "2.1.4"
+
+project.extra["PluginName"] = "Auto Log Hop"
+project.extra["PluginDescription"] = "Auto logs and hops for you <3"
+
+tasks {
+    jar {
+        manifest {
+            attributes(mapOf(
+                "Plugin-Version" to project.version,
+                "Plugin-Id" to nameToId(project.extra["PluginName"] as String),
+                "Plugin-Provider" to project.extra["PluginProvider"],
+                "Plugin-Description" to project.extra["PluginDescription"],
+                "Plugin-License" to project.extra["PluginLicense"]
+            ))
+        }
+    }
+}
